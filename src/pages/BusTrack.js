9b9a007a333a7c1c1bb81c994e38c8dc9dd23f09
Julee03 <86723152+Julee03@users.noreply.github.com>
2024-04-11 00:55:48 +0530

    Update BusTrack.js

--- src/pages/BusTrack.js
+++ src/pages/BusTrack.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import '../css/About.css';
 
 const BusTrack = () => {
   return (
-    <div>BusTrack</div>
+    <div className='about'>
+      <div className='abt_heading'>
+        <h1><span>BUS </span> Tracking</h1>
+      </div>
+    </div>
   )
 }
 
